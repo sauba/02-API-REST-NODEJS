037e8a830be7c0dd05ca5d728e1905c85d13b73a
Thiago José de Queiroz <thiagojqueiroz@outlook.com>
2023-07-11 11:14:16 -0300

    FIX RENDER DEPLOY: redo update server.ts and removing constants port and host

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,9 @@
 import { app } from './app'
 import { env } from './env'
 
-const port = env.PORT
-const host = 'RENDER' in process.env ? '0.0.0.0' : 'localhost'
-
 app
   .listen({
-    port,
-    host,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP SERVER RUNNING!')
